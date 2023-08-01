9ef06d63c3f96ba8c5cfd69c2c69fe09317db237
mjcoding0702 <chungmangjie2000@gmail.com>
2023-08-01 22:38:53 +0800

    Done Homepage UI

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -2,21 +2,88 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactPlayer from 'react-player';
 
+
+function Chip({text, isActive}){
+  return (
+    <>
+      {(isActive)? 
+      (
+        <span className="badge rounded text-white bg-black fw-normal fs-6 me-3" style={{padding: '10px 15px'}}>{text}</span>
+      ): 
+      (
+         <span className="badge rounded text-black fw-normal fs-6 me-3" style={{padding: '10px 15px', backgroundColor:'rgba(0,0,0,0.05)'}}>{text}</span>
+      )}
+    </>
+  )
+}
+
+function VideoCard({imageURL,logoURL,title,user,views,uploaded_at}){
+  return (
+    <>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xxl-3">
+          <p href="#" className="card card-video border-0 bg-transparent mb-4">
+          <div className="image-container" style={{height: '200px', overflow: 'hidden'}}>
+              <img src={imageURL} className="img-fluid" alt="test" style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+          </div>
+              <div className="card-video-details d-flex mt-2">
+                  <div className='me-3'>
+                      <img src={logoURL} width="40" alt='test' className='rounded-circle'/>
+                  </div>
+                  <div>
+                      <span className='fw-medium'>{title}</span>
+                      <br/>
+                      <span className='text-dark-emphasis' style={{fontSize:'14px'}}>{user}</span>
+                      <br/>
+                      <span className='text-dark-emphasis' style={{fontSize:'14px'}}>{views} Views • {uploaded_at} Months Ago</span>
+                  </div>
+              </div>
+          </p>
+      </div>
+    </>
+  )
+}
+
 export default function HomeContent() {
     return (
         <>
-            <div style={{ marginLeft: '250px', backgroundColor: '#f1f1f1' }}>
-                <div className="container">
-                    <div className="player-wrapper">
-                        <ReactPlayer
-                            className='react-player'
-                            url='https://firebasestorage.googleapis.com/v0/b/mentor-mentee-booking-system.appspot.com/o/meetings%2Fsample-5s.mp4?alt=media&token=4c92fd04-d469-479f-835c-2ef19b7f7228'
-                            width='100%'
-                            height='100%'
-                            controls={true}
-                        />
-                    </div>
-                </div>
+            <style>
+              {`
+                  @media (min-width: 576px) {
+                      .ms-sm-250 {
+                          margin-left: 250px !important;
+                      }
+                  }
+              `}
+            </style>
+
+            <div className='my-2 px-4 ms-sm-250' style={{ height: '100%'}}>
+              <div className='chip-container d-flex flex-nowrap overflow-hidden mb-4'>
+                <Chip isActive={true} text='All'></Chip>
+                <Chip text='Music'></Chip>
+                <Chip text='Podcasts'></Chip>
+                <Chip text='Finance'></Chip>
+                <Chip text='Computer Science'></Chip>
+                <Chip text='JavaScript'></Chip>
+                <Chip text='Python'></Chip>
+                <Chip text='Machine Learning'></Chip>
+                <Chip text='Data Science'></Chip>
+                <Chip text='C++'></Chip>
+                <Chip text='Manga'></Chip>
+                <Chip text='Live'></Chip>
+                <Chip text='Lessons'></Chip>
+                <Chip text='Firebase'></Chip>
+                <Chip text='Motivation'></Chip>
+                <Chip text='Watched'></Chip>
+                <Chip text='React Routers'></Chip>
+              </div>
+
+              <div className='row'>
+                  <VideoCard imageURL='src\assets\thumbnail.png' logoURL = 'src\assets\logo.png' title = 'Build 15 JavaScript Projects - Vanilla JavaScript Course' user = 'freeCodeCamp.org' views={100} uploaded_at={3}/>
+                  <VideoCard imageURL='src\assets\thumbnail.png' logoURL = 'src\assets\logo.png' title = 'Build 15 JavaScript Projects - Vanilla JavaScript Course' user = 'freeCodeCamp.org' views={100} uploaded_at={3}/>
+                  <VideoCard imageURL='src\assets\thumbnail.png' logoURL = 'src\assets\logo.png' title = 'Build 15 JavaScript Projects - Vanilla JavaScript Course' user = 'freeCodeCamp.org' views={100} uploaded_at={3}/>
+                  <VideoCard imageURL='src\assets\thumbnail.png' logoURL = 'src\assets\logo.png' title = 'Build 15 JavaScript Projects - Vanilla JavaScript Course' user = 'freeCodeCamp.org' views={100} uploaded_at={3}/>
+                  <VideoCard imageURL='src\assets\thumbnail.png' logoURL = 'src\assets\logo.png' title = 'Build 15 JavaScript Projects - Vanilla JavaScript Course' user = 'freeCodeCamp.org' views={100} uploaded_at={3}/>
+              </div>
             </div>
         </>
     );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,117 +1,39 @@
 
+function SideElement({icon, text}) {
+    return (
+        <>
+            <li className="nav-item">
+                <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
+                <i className={`${icon} bi me-3`} style={{fontSize: "24px"}}></i>
+                {text}
+                </a>
+            </li>
+        </>
+    )
+}
+
 export default function Sidebar() {
   return (
     <>
-        <div className="sidebar bg-white overflow-auto" style={{position:"sticky", height:"calc(100% - 60px)", left: 0, width:"250px"}}>
+        <div className="sidebar bg-white overflow-auto pb-5 d-none d-sm-block" style={{ position: 'fixed', height:"100vh", width:"250px"}}>
             <ul className="nav nav-pills flex-column mb-auto ps-3 px-2">
-                <li className="nav-item">
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-house-door-fill me-3" style={{fontSize: "24px"}}></i>
-                    Home
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-film me-3" style={{fontSize: "24px"}}></i>
-                    Shorts
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-collection-play me-3" style={{fontSize: "24px"}}></i>
-                    Subscription
-                    </a>
-                </li>
-               
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-play-circle me-3" style={{fontSize: "24px"}}></i>
-                    Youtube Music
-                    </a>
-                </li>
-                <hr></hr>
-                
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-clock-history me-3" style={{fontSize: "24px"}}></i>
-                    History
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-play-btn me-3" style={{fontSize: "24px"}}></i>
-                    Your videos
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-clock me-3" style={{fontSize: "24px"}}></i>
-                    Watch later
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-download me-3" style={{fontSize: "24px"}}></i>
-                    Downloads
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-hand-thumbs-up me-3" style={{fontSize: "24px"}}></i>
-                    Liked videos
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-three-dots me-3" style={{fontSize: "24px"}}></i>
-                    Show more
-                    </a>
-                </li>
-
-                <hr></hr>
-
+                <SideElement icon='bi-house-door-fill' text="Home"/>
+                <SideElement icon='bi-film' text="Shorts"/>
+                <SideElement icon='bi-collection-play' text="Subscription"/>
+                <SideElement icon='bi-play-circle' text="Youtube Music"/>
+                <hr/>
+                <SideElement icon='bi-clock-history' text="History"/>
+                <SideElement icon='bi-clock' text="Watch later"/>
+                <SideElement icon='bi-download' text="Downloads"/>
+                <SideElement icon='bi-hand-thumbs-up' text="Liked videos"/>
+                <SideElement icon='bi-three-dots' text="Show more"/>
+                <hr/>
                 <p className="ms-3 fw-medium fs-5">Explore</p>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-fire me-3" style={{fontSize: "24px"}}></i>
-                    Trending
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-file-music me-3" style={{fontSize: "24px"}}></i>
-                    Music
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-controller me-3" style={{fontSize: "24px"}}></i>
-                    Gaming
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-newspaper me-3" style={{fontSize: "24px"}}></i>
-                    News
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#" className="nav-link link-body-emphasis d-flex align-items-center">
-                    <i className="bi bi-trophy me-3" style={{fontSize: "24px"}}></i>
-                    Sports
-                    </a>
-                </li>
-
+                <SideElement icon='bi-fire' text="Trending"/>
+                <SideElement icon='bi-file-music' text="Music"/>
+                <SideElement icon='bi-controller' text="Gaming"/>
+                <SideElement icon='bi-newspaper' text="News"/>
+                <SideElement icon='bi-trophy' text="Sports"/>
                 <hr></hr>
 
                 <ul className="nav nav-footer mb-3 fw-medium ps-3">
@@ -150,8 +72,6 @@ export default function Sidebar() {
                         <a className="nav-link fw-light text-dark p-1" href="#">© 2023 Google LLC</a>
                     </li>
                 </ul>
-
-
             </ul>
         </div>
     </>
